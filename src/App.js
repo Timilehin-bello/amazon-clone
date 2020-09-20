2ad1edcd9fa6820f61e8434b3272da4dddf813a2
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2020-09-21 00:36:32 +0100

    Added react router dom to projrct

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Timmy Programmer</h1>
-    </div>
+    <BrowserRouter>
+        <div className="app">
+          <Switch>
+            <Route path="/checkout">
+              <h1>CHECKOUT PAGE</h1>
+            </Route>
+            <Route path="/login">
+              <h1>LOGIN PAGE</h1>
+            </Route>
+            <Route exact path="/">
+              <h1>HOME PAGE</h1>
+            </Route>
+          </Switch>
+        </div>
+    </BrowserRouter>
   );
 }
 
