36db056803995079fe9d4f998d711449ba3bdd96
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2020-09-21 03:12:07 +0100

    Created Header component and styled the component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Header from "./component/Header/Header";
+import Home from "./component/Home/Home";
 
 
 function App() {
   return (
     <BrowserRouter>
-        <div className="app">
-          <Switch>
-            <Route path="/checkout">
-              <h1>CHECKOUT PAGE</h1>
-            </Route>
-            <Route path="/login">
-              <h1>LOGIN PAGE</h1>
-            </Route>
-            <Route exact path="/">
-              <h1>HOME PAGE</h1>
-            </Route>
-          </Switch>
-        </div>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header  />
+            <h1>CHECKOUT PAGE</h1>
+          </Route>
+          <Route path="/login">
+            <h1>LOGIN PAGE</h1>
+          </Route>
+          <Route exact path="/">
+            <Header />
+            <Home/>
+            <h1>HOME PAGE</h1>
+          </Route>
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
